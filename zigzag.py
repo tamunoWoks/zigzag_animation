c2d1267dca525aa1d2463409727b8cfdef04ed6c
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-17 16:17:03 -0600

    Adjust indentation based on the current direction

--- zigzag.py
+++ zigzag.py
@@ -1,11 +1,17 @@
-# Import modules & initialize variables
-import time, sys
-
+# Initialize indentation settings
 indent = 0 # How many spaces to indent
 indentIncreasing = True # Whether the indentation is increasing or not
 
 try:
-    while True: # The main program loop
-        print(' ' * indent, end='')
-        print('********')
+    while True: # The main program infinite loop to keep animation running
+        # Print the current indentation followed by the asterisks on the same line
+        print(' ' * indent, end='') # Print spaces without newline
+        print('********') # Print the asterisks with newline
+
         time.sleep(0.1) # Pause for 1/10th of a second.
+
+        # Adjust indentation based on the current direction
+        if indentIncreasing:
+            indent += 1  # Move to the right by increasing indentation
+            if indent == 20: # If it reaches the far right limit
+                indentIncreasing = False  # Change direction 
