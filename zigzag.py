3a664caf57b655b77640cb4597e646d64bb16aa3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-17 16:12:01 -0600

    Main program loop

--- zigzag.py
+++ zigzag.py
@@ -3,3 +3,9 @@ import time, sys
 
 indent = 0 # How many spaces to indent
 indentIncreasing = True # Whether the indentation is increasing or not
+
+try:
+    while True: # The main program loop
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1) # Pause for 1/10th of a second.
