6d259716613f6b872f2b92332ea73ea965061da2
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-20 19:58:22 -0600

    Optimize while loop

--- zigzag.py
+++ zigzag.py
@@ -12,22 +12,21 @@ indent = 0 # How many spaces to indent
 indentIncreasing = True # Whether the indentation is increasing or not
 
 try:
-    while True: # The main program infinite loop to keep animation running
-        # Print the current indentation followed by the asterisks on the same line
-        print(' ' * indent, end='') # Print spaces without newline
-        print('********') # Print the asterisks with newline
+    while True:
+        # Print the indented string on a new line
+        print(' ' * indent + DISPLAY_STRING)
 
-        time.sleep(0.1) # Pause for 1/10th of a second.
+        time.sleep(DELAY)  # Control animation speed
 
-        # Adjust indentation based on the current direction
-        if indentIncreasing:
-            indent += 1  # Move to the right by increasing indentation
-            if indent == 20: # If it reaches the far right limit
-                indentIncreasing = False  # Change direction 
+        # Adjust indentation based on direction
+        if increasing:
+            indent += 1
+            if indent == MAX_INDENT:
+                increasing = False  # Change direction at rightmost point
         else:
-            indent -= 1  # Move to the left by decreasing indentation
-            if indent == 0:  # If it reaches the far left limit
-                indentIncreasing = True  # Change direction to move right again
+            indent -= 1
+            if indent == 0:
+                increasing = True   # Change direction at leftmost point
 
 # Allow the user to interrupt the animation with Ctrl+C
 except KeyboardInterrupt:
